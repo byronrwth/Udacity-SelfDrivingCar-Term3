ecaae66fd825bf7da80adb673a56668974b41f2b
xi cheng <byroneagle@gmail.com>
2018-01-10 16:00:10 +0100

    start p2

--- P2-Semantic-Segmentation/main.py
+++ P2-Semantic-Segmentation/main.py
@@ -33,7 +33,13 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+
+    tf.save_model().loader.load(sess, [vgg_tag], vgg_tag)
+    graph = tf.get_default_graph()
+    w1 = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep = graph.get_tensor_by_name(vgg_input_tensor_name)
+
+    return w1, keep, None, None, None
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -47,6 +53,12 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
+    conv_1x1 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding= 'same', kernel_regularizer=tf.contib.layers.l2_regularizer(1e-3))
+
+    output = tf.layers.conv2d_transpose(conv_1x1, num_classes, 4, 2, padding='same', kernel_regularizer=tf.contib.layers.l2_regularizer(1e-3))
+
+    tf.Print(output, [tf.shape(output)[1:3]])
+
     return None
 tests.test_layers(layers)
 
@@ -61,6 +73,8 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
     # TODO: Implement function
+
+    logits = tf.reshape(input, (-1, num_classes))
     return None, None, None
 tests.test_optimize(optimize)
 
@@ -81,6 +95,10 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param learning_rate: TF Placeholder for learning rate
     """
     # TODO: Implement function
+    for epoch in epochs:
+        for image, label in get_batches_fn(batch_size):
+            #training
+            pass
     pass
 tests.test_train_nn(train_nn)
 
@@ -109,6 +127,8 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        input_image, keep_prob, layer3_out, layer4_out, layer7_out = load_vgg(sess, vgg_path)
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
 
         # TODO: Train NN using the train_nn function
 
